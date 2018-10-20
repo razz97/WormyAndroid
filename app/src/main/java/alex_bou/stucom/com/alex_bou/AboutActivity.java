31faf0f87cf7e61e08e6c34b717274c0f063afd4
razz97 <5alexbouoliete@gmail.com>
2018-10-20 17:28:06 +0200

    about landscape, maps activity

--- app/src/main/java/alex_bou/stucom/com/alex_bou/AboutActivity.java
+++ app/src/main/java/alex_bou/stucom/com/alex_bou/AboutActivity.java
@@ -1,7 +1,10 @@
 package alex_bou.stucom.com.alex_bou;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Button btnMap = findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToPlay = new Intent(AboutActivity.this,MapsActivity.class);
+                startActivity(goToPlay);
+            }
+        });
+
     }
 }
