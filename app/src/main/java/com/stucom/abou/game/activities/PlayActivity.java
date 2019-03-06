8b7278307758759492f653cd59eebf8ba88009d5
razz97 <5alexbouoliete@gmail.com>
2019-03-06 15:32:10 +0100

    wormy view

--- app/src/main/java/com/stucom/abou/game/activities/PlayActivity.java
+++ app/src/main/java/com/stucom/abou/game/activities/PlayActivity.java
@@ -7,32 +7,50 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.stucom.abou.game.activities.bootstrap.StartActivity;
 import com.stucom.abou.game.rest.AccessApi;
+import com.stucom.abou.game.views.WormyView;
 
 import java.util.regex.Pattern;
 
 import alex_bou.stucom.com.alex.R;
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity implements WormyView.WormyListener {
 
     TextInputLayout input ;
     TextInputLayout inputLevel;
     Button button;
+    private WormyView wormyView;
+    private TextView tvScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        wormyView = findViewById(R.id.wormyView);
+        Button btnNewGame = findViewById(R.id.btnNewGame);
+        tvScore = findViewById(R.id.tvScore);
+        btnNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvScore.setText("0");
+                wormyView.newGame();
+            }
+        });
+        wormyView.setWormyListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        /*
         input = findViewById(R.id.inputScore);
         inputLevel = findViewById(R.id.inputLevel);
         button = findViewById(R.id.buttonSubmit);
@@ -42,7 +60,28 @@ public class PlayActivity extends AppCompatActivity {
                 if (validateLevel() && validateScore())
                     submitScore();
             }
-        });
+        }); */
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        switch(event.getKeyCode()) {
+            case KeyEvent.KEYCODE_A: wormyView.update(0, +10); break;
+            case KeyEvent.KEYCODE_Q: wormyView.update(0, -10); break;
+            case KeyEvent.KEYCODE_O: wormyView.update(-10, 0); break;
+            case KeyEvent.KEYCODE_P: wormyView.update(+10, 0); break;
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public void scoreUpdated(View view, int score) {
+        tvScore.setText(String.valueOf(score));
+    }
+
+    @Override
+    public void gameLost(View view) {
+        Toast.makeText(this, getString(R.string.you_lost), Toast.LENGTH_LONG).show();
     }
 
     private boolean validateScore() {
